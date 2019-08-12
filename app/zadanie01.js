8b4d2a14fff4cd80fa8cbb0adf960f69f2595871
natkalia <natalia.banasiewicz@wp.pl>
2019-08-12 11:31:42 +0200

    exercise zadanie01 solved

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,17 @@
-//Twój kod
+// Dodawanie ze ścieżki
+// Stwórz taką aplikację Express, która potrafi przyjąć w ścieżce dwie liczby. Następnie odbierz je, zsumuj, a sumę wyświetl w przeglądarce.
+// Pamiętaj, że parametry są zwracane jako string. Potrzebujesz więc użyć np. parseInt(), żeby zmienić je w liczby.
+
+const express = require('express');
+const app = express();
+
+app.get('/:first/:second', (req, res) => {
+  const first = Number(req.params.first);
+  const second = Number(req.params.second);
+  let sum = first + second;
+  res.send(`Sum of ${first} and ${second} is: ${sum}.`);
+});
+
+app.listen(3000, () => {
+  console.log('Serwer uruchomiony na porcie 3000');
+});
